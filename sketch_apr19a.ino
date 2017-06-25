4dd4c899899415540805d89ff8fc75bfdf5cd3db
vinibernardo <vinicius_pg@hotmail.com.br>
2017-06-25 17:33:32 -0400

    Arduino

--- sketch_apr19a.ino
+++ sketch_apr19a.ino
@@ -1,33 +1,55 @@
 void setup() {
-  pinMode(6,OUTPUT); 
+  pinMode(6,OUTPUT);
   pinMode(7,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(4,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
   Serial.begin(9600);
-  
+
 }
 int leitura;
 
 void loop() {
-  if(Serial.available()){
-    leitura = Serial.read();
-    
-    if(leitura == 49){
-       digitalWrite(6,LOW);
-    }else if(leitura == 50){
+  
+    if(Serial.available()) {
+      
+      leitura = Serial.read();
+      // ligar ventilador 1
+      if (leitura == 49) {
         digitalWrite(6,HIGH);
-    }else if(leitura == 51){
+      }
+      // desligar ventilador 2
+      else if (leitura == 50) {
+        digitalWrite(6,LOW);
+      }
+      // ligar luz_da_sala 3
+      else if (leitura == 51) {
         digitalWrite(7,HIGH);
-    }else if(leitura == 52){
+      }
+      // desligar luz da sala 4
+      else if (leitura == 52) {
         digitalWrite(7,LOW);
-    }else if(leitura == 55){
-        digitalWrite(5,HIGH);
-    }else if(leitura == 56){
-        digitalWrite(5,LOW);
-    }else if(leitura == 53){
-        digitalWrite(4,HIGH);
-    }else if(leitura == 54){
-        digitalWrite(4,LOW);
-    } 
+      }
+
+      // ligar tv 5
+      else if (leitura == 53) {
+        digitalWrite(8,HIGH);
+      }
+
+      // desligar tv 6
+      else if (leitura == 54) {
+        digitalWrite(8,LOW);
+      }
+
+      // desligar luz_quarto 7
+      else if (leitura == 55) {
+        digitalWrite(9,HIGH);
+      }
+
+      // desligar luz_quarto 8
+      else if (leitura == 56) {
+        digitalWrite(9,LOW);
+      }
+    
+    }
   }
-}
+
